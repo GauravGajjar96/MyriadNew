b816153cbc8eb1b33a12c0a620446ac1618f849d
PARTHIV98 <parthivpatel14398@gmail.com>
2022-05-26 11:44:13 +0530

    Changes

--- src/components/FlexibleComponents/ContentWithSidebar.tsx
+++ src/components/FlexibleComponents/ContentWithSidebar.tsx
@@ -58,7 +58,7 @@ function ContentWithSidebar({ QueryData }: Props): JSX.Element {
               <div className={`${styles.sidebarCol} col`}>
                 <ul className={styles.sidebarlinks}>
                   {os_links?.map((oslink) => {
-                    console.log(oslink.url,router.pathname,router);
+                    console.log(oslink.url.substring(oslink.url.lastIndexOf('/') + 2),router.pathname,router);
                     return(
                     <li key={`${oslink.label}1`}>
                       <Link href={oslink.url ?? ""}>
